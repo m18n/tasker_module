a3f0ff4f243d7e0ffb9ff981ee99ca9c7771b299
m5m14 <20xim04@gmail.com>
2023-11-11 18:11:13 +0200

    fix all

--- include/tasker_node.h
+++ include/tasker_node.h
@@ -5,7 +5,11 @@
 class tasker_node {
 public:
   tasker_node() {
+    
+      web.set_name_server("tasker");
+      web.add_conn_server("127.0.0.1:3001");
       web.start_server(3000);
+      
   }
   
 private:
