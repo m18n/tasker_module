76582105047bd86b1217dc1a24e96edae3e9da59
m5m14 <20xim04@gmail.com>
2023-11-11 16:01:38 +0200

    edit structure project

--- include/tasker_node.h
+++ include/tasker_node.h
@@ -1,26 +1,14 @@
 #pragma once
-#include "url.h"
 
 #include <iostream>
-#include"connector_manager.h"
+#include"web_server.h"
 class tasker_node {
 public:
   tasker_node() {
-    //controller::tg=&tg;
-    url::init_api_url(tasker_node_api);
-    int n=0;
-    while(server::isPortOccupied(3000+n)==true){
-      n++;
-    }
-    
-    tasker_node_api.port(3000+n).run();
+      web.start_server(3000);
   }
-  void telegram_auth(){
-    
-  }
+  
 private:
-  //logintg tg;
-  crow::SimpleApp tasker_node_api;
-  connector::connector_manager conn;
+  server::web_server web;
  
 };
--- include/url.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include"controller.h"
-
-namespace url{
-void init_api_url(crow::SimpleApp& app);
-}
