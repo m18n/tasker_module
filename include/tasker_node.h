82d3e07bba8b60517eb507f74d5ebc7a0270f08d
m5m14 <20xim04@gmail.com>
2023-11-14 23:59:40 +0200

    add loger

--- include/tasker_node.h
+++ include/tasker_node.h
@@ -5,7 +5,8 @@
 class tasker_node {
 public:
   tasker_node() {
-    
+      connector::init_logg_connector(&log_conn);
+      server::init_logg_server(&log_serv);
       web.set_name_server("tasker");
       web.add_conn_server("127.0.0.1:3001");
       web.start_server(3000);
@@ -13,6 +14,8 @@ public:
   }
   
 private:
+  connector::Logger log_conn;
+  server::Logger log_serv;
   server::web_server web;
  
 };
